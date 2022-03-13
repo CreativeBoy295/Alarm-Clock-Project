61ed20eda95ffc806d48c5c1ffe2815e84ea1e9a
CreativeBoy295 <you@example.com>
2022-03-13 14:26:48 +0300

    Adding and Setup ESP32Time Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <ESP32Time.h>
 
 
 #define NUM_LEDS  64
 #define LED_PIN   4
 
 CRGB leds[NUM_LEDS];
+ESP32Time rtc;
 
 void setup() {
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(50);
+  Serial.begin(115200);
+
 }
 
 void loop() {
+  
 }
